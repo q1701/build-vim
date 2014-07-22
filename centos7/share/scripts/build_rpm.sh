8b6fbd12dc3889c35615e9cb773547aaf0822f6b
q1701 <q1701@outlook.com>
2014-07-22 11:23:06 +0900

    Support CentOS 7

---
+++ centos7/share/scripts/build_rpm.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Abort if error.
+trap "exit 1" ERR
+
+#======================================
+# Build a Vim's RPM file (GUI enabled)
+#======================================
+
+# Environments
+export SCRIPT_DIR=$VOLUME_SHARE_CONTAINER/scripts
+export OUT_DIR=$VOLUME_SHARE_CONTAINER
+
+# Update yum packages
+yum -y update
+
+# Install the checkinstall
+yum -y install  $(ls $SCRIPT_DIR/checkinstall*.rpm | tail -1)
+# Prepare environment to run the rpm-build.
+export HOME=/root
+mkdir -p $HOME/rpmbuild/SOURCES
+
+# Workaround for (Can't open perl script "/usr/share/perl5/ExtUtils/xsubpp")
+# http://ciarand.me/posts/compiling-vim-on-fedora-20/
+ln -s /usr/bin/xsubpp /usr/share/perl5/ExtUtils/xsubpp
+
+# Download the original version
+yum -y install mercurial
+hg clone https://vim.googlecode.com/hg/ vim
+cd $BUILD_TMP/vim
+
+# Build
+## Configure
+./configure \
+  --prefix=/usr/local \
+  --with-features=huge \
+  --enable-multibyte \
+  --enable-xim \
+  --enable-fontset \
+  --enable-fail-if-missing \
+  --disable-darwin \
+  --disable-selinux \
+  --with-x \
+  --enable-gui=gnome2 \
+  --enable-luainterp \
+  --enable-perlinterp \
+  --enable-pythoninterp \
+  --enable-rubyinterp \
+  --enable-cscope
+## Make and install
+make install
+## Identify the version string. ("x.y.z")
+export VERSION=""$(LANG=C vim --version | grep "^VIM" | sed "s/VIM - Vi IMproved \([0-9]*\.[0-9]*\).*/\1/").$(LANG=C vim --version | grep "^Included patches:" | sed "s/Included patches:.*-\([0-9]*\)/\1/")""
+## Build a rpm.
+export REQUIRES=gtk2,libSM,libXt,lua,perl-libs,python-libs,ruby-libs
+echo "Vim $VERSION" > description-pak
+checkinstall --type=rpm --pkgname=vim --pkgversion=$VERSION --default --requires=$REQUIRES --autoreqprov=no
+## Save the full path of the rpm file into a file
+export RPM_PATH="$HOME/rpmbuild/RPMS/$(arch)/vim-$VERSION-1.$(arch).rpm"
+## Install the generated rpm file to test it
+yum -y localinstall $RPM_PATH
+cd $BUILD_TMP
+
+# Export
+# (Environment "VOLUME_SHARE_CONTAINER" must be specified when 'docker run'.)
+cp -p $RPM_PATH $OUT_DIR
+rm -f $RPM_PATH
+
+echo "========================="
+echo "Created: $(basename $RPM_PATH)"
+echo "========================="
+
+# Clean up
+cd /
+yum clean all
+rm -rf $BUILD_TMP
